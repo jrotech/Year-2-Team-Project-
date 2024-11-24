0870f7e5f4a090e5a27e03f7bf22fc8889c7148d
Mihail <mishgun032@yandex.com>
2024-11-24 23:30:35 +0000

    update shop page title

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -4,16 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Shop</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="">
+    <body class="h-[200vh]">
       <div id="nav"></div>
     </body>
 </html>
