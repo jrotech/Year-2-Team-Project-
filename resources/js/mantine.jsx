2775390620353da121f7e347b812f98040802f7d
Mihail <mishgun032@yandex.com>
2024-11-25 19:49:24 +0000

    moving mantine theme object to separat file

---
+++ resources/js/mantine.jsx
@@ -0,0 +1,19 @@
+import { createTheme } from '@mantine/core';
+
+export const theme = createTheme({
+  colors: {
+    main: [
+      "#010035",
+      "#1A1947",
+      "#1A1947",
+      "#1A1947",
+      "#1A1947",
+      "#1A1947",
+      "#1A1947",
+      "#1A1947",
+      "#1A1947",
+      "#1A1947",
+    ],
+  },
+  primaryColor: "main",
+});
