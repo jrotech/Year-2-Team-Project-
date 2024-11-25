6ac1d005270bf9c800e80f705dba8bf758d6edce
Mihail <mishgun032@yandex.com>
2024-11-25 20:56:50 +0000

    extracted in stock indicator to separat component

---
+++ resources/js/components/InStock.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Flex, Title } from '@mantine/core'
+
+export function InStock({inStock}){
+  return (
+    <Flex className="items-baseline gap-2">
+      <div className={`${!inStock ? "bg-red-600" : "bg-main-green"} w-3 h-3 rounded-full`}></div>
+      <Title order={4}>{!inStock && "not"} in stock</Title>
+    </Flex>
+  )
+  
+}
