495664cf02a140abd1363a30cd2bb39bd07b76cd
Mihail <mishgun032@yandex.com>
2024-11-25 22:11:05 +0000

    making svgs larger

--- resources/js/shop/Product.jsx
+++ resources/js/shop/Product.jsx
@@ -7,7 +7,7 @@ export default function Product({name,img,price,inStock,rating,id,wishList}){
   return (
     <Stack className="bg-white w-80 rounded-md relative px-12 py-8 gap-7 hover:bg-main-bg transition-color duration-300">
       <div className="absolute top-3 right-3 cursor-pointer">
-	<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24" stroke="currentColor" fill={wishList ? "#FF0707" : "none"} strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="hover:fill-[#FF0707] transition-colors duration-300 icon icon-tabler icons-tabler-outline icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
+	<svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24" stroke="currentColor" fill={wishList ? "#FF0707" : "none"} strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="hover:fill-[#FF0707] transition-colors duration-300 icon icon-tabler icons-tabler-outline icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
       </div>
       <Title order={2} className="text-center">{name}</Title> 
       <img alt="" src={img} className="w-40" />
