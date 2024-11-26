acbefcc25fecf847ccc6d2b7685ec03be288936a
Mihail <mihailv032@pm.m>
2024-11-26 18:51:56 +0000

    fixing pk for invoices table

--- database/migrations/2024_11_26_155334_create_invoices_table.php
+++ database/migrations/2024_11_26_155334_create_invoices_table.php
@@ -9,16 +9,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('invoices', function (Blueprint $table) {
-            $table->id(); // primary key
             $table->string('status', 20)->default('pending'); // status: pending, paid
             $table->unsignedBigInteger('customer_id'); // foreign key -> customers table
-            $table->string('invoice_number', 50)->unique();
+            $table->unsignedBigInteger('invoice_number', 50)->autoIncrement();
             $table->date(column: 'paid_date')->nullable();
             $table->decimal('amount', 10, 2);
             $table->date('due_date')->nullable();
             $table->timestamps();
 
             $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->primary(['invoice_number']);
+
         });
     }
 
