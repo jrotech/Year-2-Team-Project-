fc5abf650e969d5899cc33eba6fa8ced2c60f768
Mihail <mihailv032@pm.m>
2024-11-26 15:56:15 +0000

    adding blade route for shop page

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,7 +1,19 @@
-<h1>Shop</h1>
-@if($searchQuery)
-    <p>Search Results for: <strong>{{ $searchQuery }}</strong></p>
-@endif
-@if($category)
-    <p>Category: <strong>{{ $category }}</strong></p>
-@endif
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="">
+      <div id="nav"></div>
+    </body>
+</html>
