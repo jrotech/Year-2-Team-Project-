b4ce9d5ae17152aa32fe7118aeea78b013392bf5
Mihail <mishgun032@yandex.com>
2024-12-07 20:01:25 +0000

    paassing backend data to frontend

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="h-[200vh] bg-main-bg">
       <div id="nav"></div>
-      <div id="product"></div>
+      <div id="product" data-product="{{ json_encode($product) }}" ></div>
       
     </body>
 </html>
