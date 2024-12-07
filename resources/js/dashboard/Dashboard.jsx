7e3e0c21797c4a91d4c658cd459e8200e512c1cf
Mihail <mishgun032@yandex.com>
2024-12-07 01:20:24 +0000

    adding related & reate product on dashboard page

--- resources/js/dashboard/Dashboard.jsx
+++ resources/js/dashboard/Dashboard.jsx
@@ -6,77 +6,93 @@ import { MantineProvider, Flex, Stack, Title } from '@mantine/core';
 import { theme } from '../mantine';
 import Profile from './Profile'
 import Contact from './Contact'
+import Review from './Rate'
+import Related from '../components/Related'
 
 function Dashboard(props){
   console.log(props)
   return (
     <MantineProvider theme={theme}>
-      <Flex className="max-w-screen justify-between m-24 relative">
+      <Flex className="max-w-screen justify-between m-32 relative">
 	<Flex className="items-center justify-center w-full">
 	  <Stack className="gap-20 flex-wrap justify-center mx-5">
 	    <Title className="text-main-accent underline">Profile</Title>
 	    <Profile name="asdf" email="asdf@sadf@" address="asdf" phone="432423432" orders="5" spent="1353" points="104343" />
-
+	    
 	    <Title className="text-main-accent underline" mt="40">Recent Orders</Title>
 	    <Stack gap="50">
-	    {
-	    Array.from({length: 3}, (_,i) => <Order key={i}
+	      {
+		Array.from({length: 3}, (_,i) => <Order key={i}
 							name={"Nvidia Gforce Rtx 3070 TI"}
 							img={"https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3070/geforce-rtx-3070-shop-600-p@2x.png"}
-						      id={i}
+							    id={i}
 							order_date={"2021-10-10"}
-						      total={599.99}
-						      products={[
-						      {
-							"name": "Nvidia Gforce Rtx 3070 TI",
-							"order_date": "2021-10-10",
-							"delivery_date": "2021-10-15",
-							"quantity": 1,
-							"unit_price": 599.99,
-							"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
-						      },
-						      {
-							"name": "Nvidia Gforce Rtx 3070 TI",
-							"order_date": "2021-10-10",
-							"delivery_date": "2021-10-15",
-							"quantity": 1,
-							"unit_price": 599.99,
-							"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
-						      },
-						      {
-							"name": "Nvidia Gforce Rtx 3070 TI",
-							"order_date": "2021-10-10",
-							"delivery_date": "2021-10-15",
-							"quantity": 1,
-							"unit_price": 599.99,
-							"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
-						      },
-						      {
-							"name": "Nvidia Gforce Rtx 3070 TI",
-							"order_date": "2021-10-10",
-							"delivery_date": "2021-10-15",
-							"quantity": 1,
-							"unit_price": 599.99,
-							"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
-						      },
-						      {
-							"name": "Nvidia Gforce Rtx 3070 TI",
-							"order_date": "2021-10-10",
-							"delivery_date": "2021-10-15",
-							"quantity": 1,
-							"unit_price": 599.99,
-							"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
-						      }
-
-						      ]}
-						      price={599.99}
-						      inStock={false}
+							    total={599.99}
+							    products={[
+							      {
+								"name": "Nvidia Gforce Rtx 3070 TI",
+								"order_date": "2021-10-10",
+								"delivery_date": "2021-10-15",
+								"quantity": 1,
+								"unit_price": 599.99,
+								"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
+							      },
+							      {
+								"name": "Nvidia Gforce Rtx 3070 TI",
+								"order_date": "2021-10-10",
+								"delivery_date": "2021-10-15",
+								"quantity": 1,
+								"unit_price": 599.99,
+								"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
+							      },
+							      {
+								"name": "Nvidia Gforce Rtx 3070 TI",
+								"order_date": "2021-10-10",
+								"delivery_date": "2021-10-15",
+								"quantity": 1,
+								"unit_price": 599.99,
+								"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
+							      },
+							      {
+								"name": "Nvidia Gforce Rtx 3070 TI",
+								"order_date": "2021-10-10",
+								"delivery_date": "2021-10-15",
+								"quantity": 1,
+								"unit_price": 599.99,
+								"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
+							      },
+							      {
+								"name": "Nvidia Gforce Rtx 3070 TI",
+								"order_date": "2021-10-10",
+								"delivery_date": "2021-10-15",
+								"quantity": 1,
+								"unit_price": 599.99,
+								"img_url": "https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png"
+							      }
+							      
+							    ]}
+							    price={599.99}
+							    inStock={false}
 							wishList={false}
 							id={i}
-					       />)
-	    }
+						 />)
+	      }
 	    </Stack>
-	    <Contact/>
+	    <Stack className="py-10">
+	      <Title className="text-center !text-5xl" mb="10" order={1}>Reviews</Title>
+	      <Review img_url="https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png" name="Nvidia Gforce Rtx 3070 TI" />
+	    </Stack>
+	    <Stack>
+	      <Title className="text-center !text-5xl" mb="10" order={1}>Related Products</Title>
+	      <Flex gap="20">
+		{
+		  Array.from({length: 3}, (_,i) => <Related key={i} name="yes" price="200" id={i+1}
+							    description="Condimentum mattis pellentesque id nibh tortor, id aliquet lectus proin nibh nisl"
+							    img_url="https://www.broadberry.co.uk/img/gpu-compare/titanrtx.png" />)
+		}
+	      </Flex>
+	    </Stack>
+	    <Contact phone_number="068334" />
 	  </Stack>
 	</Flex>
 	<Sidebar />
--- resources/js/dashboard/Profile.jsx
+++ resources/js/dashboard/Profile.jsx
@@ -3,7 +3,7 @@ import { Stack, Avatar, Title, Flex } from '@mantine/core'
 
 export default function Profile(props){
   return (
-    <Flex className="bg-white rounded-md p-10 gap-24">
+    <Flex className="bg-white rounded-md p-10 gap-24 w-[800px]">
       <Avatar radius="xl" size="xl" />
       <Stack>
 	<Title order={4}>Full Name: {props.name}</Title>
