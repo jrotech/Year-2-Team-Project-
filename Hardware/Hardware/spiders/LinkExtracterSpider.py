e7cb9b6853e3824beba7db77f0b45970f2694e9f
Robert Oros <jacob.oros@hotmail.com>
2025-02-18 23:43:33 +0000

    Extract link of list item

--- Hardware/Hardware/spiders/LinkExtracterSpider.py
+++ Hardware/Hardware/spiders/LinkExtracterSpider.py
@@ -5,6 +5,15 @@ class LinkextracterspiderSpider(scrapy.Spider):
     name = "LinkExtracterSpider"
     allowed_domains = ["www.scan.co.uk"]
     start_urls = ["https://www.scan.co.uk/shop/computer-hardware/cpu-amd-desktop/all"]
-
+    
     def parse(self, response):
-        pass
+        
+        category = response.css('div.category')
+        for cat in category:
+            ulist = cat.css("ul.product-group")
+            for ul in ulist:
+                for li in ul.css("li.product"):
+                    yield {
+                        "link": li.css("a.image::href").extract_first()
+                    }
+        
