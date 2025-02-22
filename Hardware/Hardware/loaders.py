cb24a618366dcfbd5d9374ddcf1f035327546cd0
Robert Oros <jacob.oros@hotmail.com>
2025-02-22 13:28:10 +0000

    added CPU loader

--- Hardware/Hardware/loaders.py
+++ Hardware/Hardware/loaders.py
@@ -10,3 +10,14 @@ def extract_tdp(value):
     #regex for '350W' -> '350'
     match = re.search(r"(\d+)\s*W", value)
     return match.group(1) if match else value
+
+class CPULoader(ItemLoader):
+    default_output_processor = TakeFirst()  
+
+    name_in = MapCompose(clean_text)
+    price_in = MapCompose(clean_price)
+    description_in = MapCompose(clean_text)
+    socket_type_in = MapCompose(clean_text)
+    tdp_in = MapCompose(clean_text, extract_tdp)  
+    integrated_graphics_in = MapCompose(clean_text)
+    image_urls_out = Join(",")  # Joins list of image URLs into a string
