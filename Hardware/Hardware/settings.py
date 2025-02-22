129bd96bb29922bbf605a7e81dbc3d8346b1d4be
Robert Oros <jacob.oros@hotmail.com>
2025-02-22 12:55:34 +0000

    Bypassing Robotstxt and changing user agent

--- Hardware/Hardware/settings.py
+++ Hardware/Hardware/settings.py
@@ -14,10 +14,10 @@ NEWSPIDER_MODULE = "Hardware.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "Hardware (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
