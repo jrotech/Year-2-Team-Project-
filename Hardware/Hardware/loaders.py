eca475c743b2f2f58e0ef910b3e8f92c2afe5ef2
Robert Oros <jacob.oros@hotmail.com>
2025-02-22 13:26:19 +0000

    more loader functions

--- Hardware/Hardware/loaders.py
+++ Hardware/Hardware/loaders.py
@@ -4,3 +4,9 @@ import re
 
 def clean_price(value):
     return value.replace("£", "").replace(",", "").strip()
+def clean_text(value):
+    return value.strip()
+def extract_tdp(value):
+    #regex for '350W' -> '350'
+    match = re.search(r"(\d+)\s*W", value)
+    return match.group(1) if match else value
