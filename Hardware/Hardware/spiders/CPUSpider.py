e0242b2f10a404e3f657c2809ab5f687e308538b
Robert Oros <jacob.oros@hotmail.com>
2025-02-22 13:30:01 +0000

    Parse Json to get correct links

--- Hardware/Hardware/spiders/CPUSpider.py
+++ Hardware/Hardware/spiders/CPUSpider.py
@@ -1,10 +1,22 @@
 import scrapy
-
+from scrapy.loader import ItemLoader
+from Hardware.items import CPUItem
+from Hardware.loaders import CPULoader
+import json
 
 class CPUSpider(scrapy.Spider):
     name = "CPUSpider"
     allowed_domains = ["scan.co.uk"]
     
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        json_file = "links.json"
+        with open(json_file, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        cpulinks =[]
+        for key in data:
+            if data[key] == "CPU":
+                cpulinks.append(key)
+        for link in cpulinks:
+            full_url = f"https://www.scan.co.uk{link}"
+            yield scrapy.Request(url=full_url, callback=self.parse)
