c7172b13ac3e7127cf48460421906497b3ad9834
Mihail <mihailv032@pm.m>
2025-03-18 15:08:06 +0000

    center image

--- resources/js/shop/Product.jsx
+++ resources/js/shop/Product.jsx
@@ -7,12 +7,6 @@ import { Title, Button, Flex, Stack, Rating, Center } from "@mantine/core";
 import {InStock} from '../components/InStock'
 
 //function to get path
-export function asset(path) {
-  // Ensure the path includes 'storage/' if not already present
-  const formattedPath = path.startsWith('storage/') ? path : `storage/${path}`;
-  
-  return `${"http://127.0.0.1:8000"}/${formattedPath}`;
-}
 
 export default function Product({ name, primary_image, price, inStock, rating, id, wishList }) {
   return (
@@ -21,8 +15,10 @@ export default function Product({ name, primary_image, price, inStock, rating, i
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" stroke="currentColor" fill={wishList ? "#FF0707" : "none"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="hover:fill-[#FF0707] transition-colors duration-300 icon icon-tabler icons-tabler-outline icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
       </div>
       <Title order={2} className="text-center">{name}</Title>
-      <img alt={name} src={primary_image} className="w-40" />
-
+      <Center>
+	<img alt={name} src={primary_image} className="w-40" />
+      </Center>
+      
       <Center>
         <Rating value={rating} size="xl" readOnly />
       </Center>
