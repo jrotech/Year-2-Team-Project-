9dc36dd22a3386dfbe6530cd440e3e6332b8ffaf
Mihail <mihailv032@pm.m>
2025-03-18 14:44:45 +0000

    adding admin seeder to main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PsuSeeder::class);
         $this->call(StorageSeeder::class);
         $this->call(CoolingSeeder::class);
+        $this->call(AdminSeeder::class);
         
     }
 }
