0e19845b128a8ea7c044a0e99aa79faad2d0df3a
Robert Oros <jacob.oros@hotmail.com>
2025-03-22 00:58:50 +0000

    Added product view and review test

--- tests/selenium-tests/test_02_login_user.py
+++ tests/selenium-tests/test_02_login_user.py
@@ -8,7 +8,6 @@ import pytest
 
 
 def test_02_login_user():
-    # Setup driver and wait
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=service, options=options)
@@ -19,16 +18,18 @@ def test_02_login_user():
     login_button = wait.until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Login')]")))
     login_button.click()
 
-    # Wait for login form and fill in credentials
+    # wait for login form and fill in credentials
     wait.until(EC.presence_of_element_located((By.NAME, "CustomerEmail"))).send_keys("john1@example.com")
     driver.find_element(By.NAME, "CustomerPassword").send_keys("SecurePass123")
 
-    # Click the login button
-    driver.find_element(By.XPATH, "//input[@value='Login']").click()  # Adjust if your login button has a different name or type
+    # click the login button
+    driver.find_element(By.XPATH, "//input[@value='Login']").click() 
 
-    # Assert login success – for example, redirected to dashboard or welcome message
+    # assert login success 
     found_bar = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@id='nav']")))
     assert found_bar is not None
+
+    driver.find_element(By.XPATH, "//a[contains(text(),'Shop')]").click()
 
     
 
