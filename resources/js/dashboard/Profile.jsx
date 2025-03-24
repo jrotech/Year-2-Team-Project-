0e2243c3986ec8ccc17ba31daf7c7709b7ef69c6
Mihail <mihailv032@pm.m>
2025-03-24 07:09:17 +0000

    fixed dashboard

--- resources/js/dashboard/Profile.jsx
+++ resources/js/dashboard/Profile.jsx
@@ -3,12 +3,14 @@ Developer: Mihail Vacarciuc
 University ID: 230238428
 ********************************/
 import React from 'react'
-import { Stack, Avatar, Title, Flex } from '@mantine/core'
+import { Stack, Avatar, Title, Flex, Center } from '@mantine/core'
 
 export default function Profile(props){
   return (
-    <Flex className="bg-white rounded-md p-10 gap-24  flex-col md:flex-row">
-      <Avatar radius="xl" size="xl" />
+    <Flex className="bg-white rounded-md p-10 gap-24  flex-col md:flex-row md:justify-normal justify-center">
+      <Center>
+	<Avatar radius="xl" size="xl" />
+      </Center>
       <Stack>
 	<Title order={4}>Full Name: {props.name}</Title>
 	<Title order={4}>Email: {props.email}</Title>
