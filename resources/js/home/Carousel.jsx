c269ea61da118ebb352709f2b988fd11a60e39bd
Mihail <mihailv032@pm.m>
2025-03-24 04:47:31 +0000

    fixed carousel responsivness

--- resources/js/home/Carousel.jsx
+++ resources/js/home/Carousel.jsx
@@ -6,13 +6,39 @@ import React from 'react';
 import { MantineProvider, Flex } from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
 
+import { useState, useEffect } from 'react';
+
 export default function CarouselWrap(){
-  const 
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+    
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  console.log(windowSize);
   return (
     <div className="flex w-screen md:justify-center md:items-center mt-[150px]">
     <Carousel
       withIndicators
-      height={200}
+      height={windowSize.width > 720 ? 400 : 200}
       slideSize={{ base: '100%', sm: '50%', md: '33.333333%' }}
       slideGap={{ base: 0, sm: 'md' }}
       loop
